d068ea39126ae4df9d65cb1745f0b0353463f672
Vladimir Gutierrez <vladimirgutierrez4063@gmail.com>
2021-03-05 01:18:42 -0300

    Practicas

---
+++ LlavesDicc.py
@@ -0,0 +1,16 @@
+"""
+Define una función la cual nos permita conocer todas las llaves dentro de un diccionario. La función debe cumplir con los siguientes requerimientos.
+
+La función debe tener por nombre sequences_items.
+La función debe recibir como argumento un diccionario.
+La función deberá imprimir en consola cada una de la llaves dentro del diccionario."""
+
+my_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4} 
+
+def sequences_items(my_dict):
+    x = my_dict.items()
+    print(x)
+
+print(sequences_items(my_dict))    
+
+
