aae2cf17028a94c725a5a182331b799a8fcf9c73
Vladimir Gutierrez <vladimirgutierrez4063@gmail.com>
2021-10-10 18:30:18 -0300

    Se termino el Create, falta pulir el Remove

--- Challenges/CRUD_MySQL/src/main.py
+++ Challenges/CRUD_MySQL/src/main.py
@@ -3,8 +3,55 @@
 
 from pydoc import text
 import pyodbc
+# import sqlite3
+# db_lite = ''
+
 db = pyodbc.connect(
         'DRIVER={SQL Server};SERVER=DESKTOP-4CUGQV7;DATABASE=Productos;UID=sa;PWD=contravlady')
+cursor = db.cursor()
+
+def ViewProducts():
+    try:
+        query = "SELECT * FROM CrudPyWombat"
+        cursor.execute(query)
+        lista = cursor.fetchall()
+        for i in lista:
+            print(i)
+
+    except Exception as ex:
+        print(ex)
+
+def Create():
+    try:
+        name = input("Ingrese detalle: ")
+        price = int(input("Ingrese precio: "))
+        query = "INSERT CrudPyWombat(Nombre, Precio) VALUES(?,?)"
+        cursor.execute(query, name, price)
+        cursor.commit()
+        cursor.close()
+
+    except Exception as ex:
+        print(ex)
+
+def Remove():
+    
+    try:
+        ViewProducts()
+        producDelete = input("Producto a eliminar: ")
+        query = "DELETE FROM CrudPyWombat WHERE Nombre = ?"
+        cursor.execute(query, producDelete)
+        cursor.commit()
+        filasAfectadas = cursor.fetchone()
+        #filasAfectadas = abs(cursor.rowcount)
+
+        if filasAfectadas > 0:
+            print(filasAfectadas)
+            print("Producto eliminado.") 
+        else:
+            print("Producto no encontrado.")
+
+    except Exception as ex:
+        print(ex)
 
 txt = """
 ******************
@@ -14,20 +61,17 @@ txt = """
     2 - Remove
     3 - Update
     4 - Delete
-
-******************
+    5 - Exit
 """
-def Create():
-    name = input("Ingrese detalle: ")
-    price = int(input("Ingrese precio"))
+#Create()
+Remove()
+#ViewProducts()
 
-    cursor.execute("INSERT CrudPyWombat VALUES(%s, %s):")
-
-Create()
-cursor = db.cursor()
-cursor.execute("SELECT count(*) FROM CrudPyWombat")
-
-valor = cursor.fetchval()
-
-print(valor)
-
+# opc = 0
+# while(opc != 5):
+#     print(txt)
+#     opc = int(input("Ingrese una opción: "))
+#     if(opc == 1):
+#         Create()
+#     if(opc == 2):
+#         Remove()
