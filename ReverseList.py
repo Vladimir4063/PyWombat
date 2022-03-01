295a6b9053c6d0dd22cfe9f9b23fb272ff4a33e9
Vladimir4063 <vladimirgutierrez4063@gmail.com>
2022-03-01 20:38:30 -0300

    ReverseList.py

---
+++ ReverseList.py
@@ -0,0 +1,10 @@
+
+# Dada una lista de números enteros.
+
+# [1, 2, 3, 4, 5, 6, 7 ]
+# Desarrolla un programa que permita imprimir en consola el listado a la inversa.
+
+ListNum = [1,2,3,4,5,6,2,2,3,4,5]
+
+newList = list(reversed(ListNum))
+print(newList)
